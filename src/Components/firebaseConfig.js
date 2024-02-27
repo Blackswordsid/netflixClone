b1441cac0c2f953e298e85ca2f0e66faa2606cbd
siddheshwar jadhav <siddh.jadhav11@gmail.com>
2024-02-27 14:21:50 +0530

    Add all my files

---
+++ src/Components/firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyAUkWcP5tigcYX2bnf2Gp80lmhNiKaRXKA",
+  authDomain: "netflix-fc051.firebaseapp.com",
+  projectId: "netflix-fc051",
+  storageBucket: "netflix-fc051.appspot.com",
+  messagingSenderId: "423177922272",
+  appId: "1:423177922272:web:ecc8c75dfc58262afb1285",
+  measurementId: "G-LSHYYLFRK6"
+};
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
